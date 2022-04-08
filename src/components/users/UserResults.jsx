a74c15f4a30424723ad8abc0136612e77cd759c8
Jim_Mortenson <jmortenson@socialsolutions.com>
2022-04-08 10:53:41 -0500

    Section 9 Lecture 52 inclusive

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,7 +1,11 @@
-//import React from 'react'
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
+import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
 
 function UserResults() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     fetchUsers();
   }, []);
@@ -15,13 +19,21 @@ function UserResults() {
 
       const data = await response.json();
 
-      console.log(data);
+      setUsers(data);
+      setLoading(false)
   }
 
-
-  return (
-    <div>UserResults</div>
-  )
+    if (!loading) {
+        return (
+            <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+                {users.map((user) => (
+                    <UserItem key={user.id} user={user} />
+                ))}
+            </div>
+        )
+    } else {
+        return <Spinner />
+    }
 }
 
 export default UserResults
