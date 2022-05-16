efb03bbe945fc8bb3a294945696abe634c545670
Jim_Mortenson <jmortenson@socialsolutions.com>
2022-05-16 10:29:13 -0500

    Removing references to GITHUB_TOKEN

--- src/context/github/GithubContext.jsx
+++ src/context/github/GithubContext.jsx
@@ -26,11 +26,11 @@ export const GithubProvider = ({children}) => {
             q: text,
         })
 
-        const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+        const response = await fetch(`${GITHUB_URL}/search/users?${params}`/*, {
             headers: {
               Authorization: `token ${GITHUB_TOKEN}`
             },
-        });
+        }*/);
   
         const {items} = await response.json();
   
@@ -44,11 +44,11 @@ export const GithubProvider = ({children}) => {
     const getUser = async (login) => {
         setLoading();
 
-        const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+        const response = await fetch(`${GITHUB_URL}/users/${login}`/*, {
             headers: {
               Authorization: `token ${GITHUB_TOKEN}`
             },
-        });
+        }*/);
 
         if (response.status === 404) {
             window.location = '/notfound';
@@ -71,11 +71,11 @@ export const GithubProvider = ({children}) => {
             per_page: 10
         });
 
-        const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
+        const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`/*, {
             headers: {
               Authorization: `token ${GITHUB_TOKEN}`
             },
-        });
+        }*/);
   
         const data = await response.json();
   
