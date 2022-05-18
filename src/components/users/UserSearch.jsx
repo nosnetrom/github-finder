9bd95107969b6db186b3230e75bd650ded405c38
Jim_Mortenson <jmortenson@socialsolutions.com>
2022-05-18 16:16:11 -0500

    Section 11 lesson 67 inclusive

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -8,7 +8,7 @@ function UserSearch() {
 
     const [text, setText] = useState('');
 
-    const {users, dispatch, clearUsers} = useContext(GithubContext);
+    const {users, dispatch} = useContext(GithubContext);
 
     const {setAlert} = useContext(AlertContext); 
 
@@ -46,8 +46,8 @@ function UserSearch() {
         </div>
         {users.length > 0 && (
             <div>
-                <button className="btn-ghost btn-lg" onClick={clearUsers}>
-                    Clear
+                <button className="btn-ghost btn-lg" onClick={() => dispatch({type: 'CLEAR_USERS'})}>
+                    CLEAR
                 </button>
             </div>
         )}
